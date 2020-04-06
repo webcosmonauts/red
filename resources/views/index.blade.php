5d1740039773c13b2c5db62df6a12f738b093d04
Dmytro Kardava <dima.kardava@gmail.com>
2020-04-06 23:21:19 +0200

    add favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" href="/images/logo-circular.png" />
+
         <title>{{ config('app.name') }}</title>
 
         @stack('head')

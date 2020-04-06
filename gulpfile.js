776f37493b8d775ef5a50a8ecca10856c7a5895c
Dmytro Kardava <dima.kardava@gmail.com>
2020-04-06 22:54:14 +0200

    add dist files

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+//Подключаем модули галпа
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+let uglify = require('gulp-uglify-es').default;
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const replace = require('gulp-replace');
+
+//Порядок подключения CSS файлов
+const cssFiles = [
+    './src/css/main.css',
+    './src/css/media.css'
+];
+
+//Порядок подключения JS файлов
+const jsFiles = [
+    './src/js/jquery-3.4.1.js',
+    './src/js/popper.min.js',
+    './src/js/bootstrap.min.js',
+    './src/js/616a422fe3.js',
+    './src/js/progressbar.min.js',
+    './src/js/lightbox.js',
+    './src/js/icheck.js',
+    './src/js/main.js'
+
+];
+
+// File paths
+const files = {
+    scssPath: './scss/**/*.scss',
+};
+
+//Таск на скрипты JS
+function scripts() {
+    //Шаблон для поиска файлов JS
+    //Всей файлы по шаблону './src/js/**/*.js'
+    return gulp.src(jsFiles)
+        //Обьединение файлов в один
+        .pipe(concat('script.min.js'))
+        //Минификация JS
+        .pipe(uglify())
+        //Выходная папка для скриптов
+        .pipe(gulp.dest('./build/js/'))
+        .pipe(browserSync.stream());
+}
+//Задача Sass: компилирует файл style.scss в style.css
+// Sass task: compiles the style.scss file into style.css
+function scssTask() {
+    return gulp.src(files.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([cssnano()])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream()); // put final CSS in dist folder
+}
+
+//Удалить все в указанной папке
+function clean() {
+    return del(['build/*'])
+}
+
+//Просматривать файлы
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    //Следить за JS файлами
+    gulp.watch('./src/js/**/*.js', scripts)
+    //Следить за SCSS файлами
+    gulp.watch([files.scssPath],
+        {usePolling: true}, scssTask);
+//При измененни HTML запустить синхронизацию
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}
+//Таск вызывающий функцию scripts
+gulp.task('scripts', scripts);
+//Таск для очистки папки build
+gulp.task('del', clean);
+//Таск для отслкживаня изменений
+gulp.task('watch', watch);
+//Таск вызывающий функцию scssTask
+gulp.task('scssTask', scssTask);
+//Таск для удаления файлов в папке build и запуск styles и scripts
+gulp.task('build', gulp.series(clean, gulp.parallel( scripts)));
+//Таск запускает таск build, scssTask  и watch
+gulp.task('dev', gulp.series('build', 'scssTask', 'watch'));
---
+++ resources/js/main.js
@@ -0,0 +1,62 @@
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+function lineBuilder(line){
+    $.each($(line), function(){
+        console.log($(this));
+        let bar = new ProgressBar.Line("#"+$(this).attr('id'),{
+            strokeWidth: 4,
+            easing: 'easeInOut',
+            duration: 1400,
+            color: '#FF0000',
+            trailColor: '#eee',
+            trailWidth: 4,
+            svgStyle: {width: '100%', height: '100%'},
+            from: {color: '#FF0000'},
+            to: {color: '#FF0000'}
+        });
+        bar.animate($(this).data('percent'));
+
+        let like_container = $(this).data('id');
+        $(like_container).css('left',Math.round($(this).data('percent') * 100) + "%");
+        $(like_container).children('.percent').text(Math.round($(this).data('percent') * 100) + "%");
+    });
+}
+
+function circleBuilder(circle){
+    $.each($(circle), function(){
+        console.log($(this));
+
+        let bar = new ProgressBar.Circle("#"+$(this).attr('id'), {
+            color: '#FFEA82',
+            trailColor: '#eee',
+            trailWidth: 6,
+            duration: 1400,
+            easing: 'bounce',
+            strokeWidth: 6,
+            from: {color: '#FF0000', a:0},
+            to: {color: '#FF0000', a:1},
+            // Set default step function for all animate calls
+            step: function(state, circle) {
+                circle.path.setAttribute('stroke', state.color);
+            }
+        });
+        bar.animate($(this).data('percent'));
+    });
+}
+
+$(document). ready(function(){
+    $(".sl-1").on("click", function(){
+        $(".slides li").remuveClass("active");
+        $(".slides-1").addClass("active");
+        $(".sl-1").addClass("wight");
+    });
+
+    lineBuilder(".line");
+    circleBuilder('.circles');
+});
+
+$('input').iCheck({
+    checkboxClass: 'icheckbox_polaris',
+    radioClass: 'iradio_polaris',
+});

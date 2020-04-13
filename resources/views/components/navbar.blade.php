1a83bba80c7e1e35ef6d027c71181f6bf7c81a89
Dmytro Kardava <dima.kardava@gmail.com>
2020-04-13 17:36:19 +0200

    fix logo img src

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="index.html"><img src="img/logo-final.png" alt=""></a>
+            <a class="navbar-brand" href="index.html"><img src="/images/logo-final.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
